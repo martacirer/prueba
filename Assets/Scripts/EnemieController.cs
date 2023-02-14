8914948b27d52ee393bed619f926703df716ac84
martacirer <martacirermorey@gmail.com>
2023-02-14 11:37:07 +0100

    enemigos

---
+++ Assets/Scripts/EnemieController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieController : MonoBehaviour
+{
+	public int rutina;
+	public float cronometro;
+	public Animator ani;
+	public Quaternion angulo;
+	public float grado;
+
+	public GameObject target;
+
+	void Start()
+	{
+		ani = GetComponent<Animator>();
+		target = GameObject.Find("Enemie1");
+	}
+
+	public void Comportamiento_enemigo()
+	{
+	//cuando el jugador este fuera del rango del enemigo hara rutina de caminar aleatoriamente
+		if(Vector3.Distance(transform.position, target.transform.position) >5)
+		{
+
+			ani.SetBool("run", false);
+			cronometro += 1 * Time.deltaTime;
+			if(cronometro >= 4)
+			{
+				rutina = Random.Range(0, 2);
+			}
+				switch(rutina)
+			{
+				case 0:
+					ani.SetBool("walk", false);
+					break;
+
+				case 1:
+					grado = Random.Range(0, 360);
+					angulo = Quaternion.Euler(0, grado, 0);
+					rutina++;
+					break;
+
+				case 2:
+					transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+					transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+					ani.SetBool("walk", true);
+					break;
+				}
+		}
+		else
+		{
+			var lookPos = target.transform.position - transform.position;
+			lookPos.y = 0;
+			var rotation = Quaternion.LookRotation(lookPos);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+			ani.SetBool("walk", false);
+
+			ani.SetBool("run", true);
+			transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+		}
+	}
+
+	void Update()
+	{
+		Comportamiento_enemigo();
+	}
+}
--- Assets/Scripts/PLayerController.cs
+++ Assets/Scripts/PLayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class PLayerController : MonoBehaviour
 {
-
+    float horizontalInput;
+    float verticalInput;
     public float speed = 10f;
-
+    
+   
     private Rigidbody playerRigidbody;
 
+    public float forceJump;
+    //float velocity;
 
     private void Awake()
     {
@@ -19,14 +23,47 @@ public class PLayerController : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;//Se oculta el raton
+
+        Physics.gravity *= 1;
     }
 
+    void Update()
+    {
+         horizontalInput = Input.GetAxis("Horizontal");
+         verticalInput = Input.GetAxis("Vertical");
 
+         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
+         if(Input.GetKeyDown(KeyCode.Space))
+         {
+             //velocity = forceJump;
+             //transform.Translate(new Vector3(0, gravity * forceJump, 0) * Time.deltaTime);
+             playerRigidbody.AddForce(Vector3.up * forceJump, ForceMode.Impulse);
+             
+         }
+        
+         
+         if(Input.GetKeyDown(KeyCode.LeftShift))
+         {
+            speed *= 2;
+         }
+
+           
+         if(Input.GetKeyUp(KeyCode.LeftShift))
+         {
+            speed /= 2;
+         }
+
+          
+    }
+
+ 
+    
+    /*
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+       
 
        
         //Movimiento horizontal del player
@@ -35,5 +72,5 @@ public class PLayerController : MonoBehaviour
         //Movimiento vertical del player
         playerRigidbody.AddForce(playerRigidbody.gameObject.transform.forward * speed * verticalInput);
       
-    }
+    }*/
 }
